e2e74121af62cc2978c8241feee9801ba1714903
Yuan Wang <yxw017@cs.bham.ac.uk>
2023-05-28 11:39:21 +0100

    added the required files and code

--- api_check.py
+++ api_check.py
@@ -1,33 +1,29 @@
 import requests
 import json
 
-#url = "enter heroku web app url here"
-url = "https://fastapiproject3.herokuapp.com/inference/"
+url = "https://fastapiproject3.herokuapp.com/inference"  # Update with your server's address and port
+headers = {
+    'Content-Type': 'application/json'
+}
 
+data = {
+    'age': 50,
+    'workclass': "Private",
+    'fnlgt': 234721,
+    'education': "Doctorate",
+    'education_num': 16,
+    'marital_status': "Separated",
+    'occupation': "Exec-managerial",
+    'relationship': "Not-in-family",
+    'race': "Black",
+    'sex': "Female",
+    'capital_gain': 0,
+    'capital_loss': 0,
+    'hours_per_week': 50,
+    'native_country': "United-States"
+}
 
-# explicit the sample to perform inference on
-sample =  { 'age':50,
-            'workclass':"Private",
-            'fnlgt':234721,
-            'education':"Doctorate",
-            'education_num':16,
-            'marital_status':"Separated",
-            'occupation':"Exec-managerial",
-            'relationship':"Not-in-family",
-            'race':"Black",
-            'sex':"Female",
-            'capital_gain':0,
-            'capital_loss':0,
-            'hours_per_week':50,
-            'native_country':"United-States"
-            }
+response = requests.post(url, headers=headers, data=json.dumps(data))
 
-data = json.dumps(sample)
-
-# post to API and collect response
-response = requests.post(url, data=data )
-
-# display output - response will show sample details + model prediction added
-print("response status code", response.status_code)
-print("response content:")
-print(response.json())
+print("Status Code: ", response.status_code)
+print("Response: ", response.json())
